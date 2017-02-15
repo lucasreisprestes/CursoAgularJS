b0773295967c219d0c894ae1d46d1a87b177e854
Dev Lucas E. R. Prestes <lucas.reis@ecomidia.com.br>
2017-02-15 10:33:27 -0200

    Com a função de inclusão, utilizando rotas

--- js/pessoas.js
+++ js/pessoas.js
@@ -7,12 +7,15 @@
     .config(function($routeProvider){  //Config corresponde a configuração de rotas do angular que utiliza $routeProvider
       //when => quando o usuário usar a rota...
       $routeProvider.when('/',
-          {
-              // passamos o objeto
+          {              // passamos o objeto
               templateUrl: 'listar.html'
+          })
+          .when('/pessoa/adicionar',
+          {
+              templateUrl:'adicionar.html',
+              controller: 'CtrlAdicionar' // preciso definir qual o controle irá utilizar
           });
-          
-        console.log('Feitooooooooooo');
+          console.log('Feitooooooooooo');
     })
 
     .controller('CtrlPessoas', function($scope){
@@ -24,5 +27,23 @@
             {nome: "Cleber", cidade:"Paraná"}
         ];
 
+   })
 
-  });
+   .controller('CtrlAdicionar', function($scope){
+
+      $scope.add = function (){
+          // forma 1 de fazer a inclusão no array
+          /*$scope.pessoas.push({
+              nome: $scope.pessoa.nome,
+              cidade: $scope.pessoa.cidade
+          })*/
+
+          // forma 2 de fazer a inclusão no array, pois o objeto está vindo certo do formulario
+          $scope.pessoas.push($scope.pessoa);
+
+          $scope.pessoa = '';
+          //$scope.pessoa.cidade = '';
+          $scope.result= "Registro adicionado com sucesso!!!"
+      };
+
+   });
